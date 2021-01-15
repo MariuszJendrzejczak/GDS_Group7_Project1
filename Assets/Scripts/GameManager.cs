b2fcc40381907cd2dadac42d054f4c2ca87be370
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-15 21:46:03 +0100

    Checkpointy

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get { return _instance; }
+    }
+
+    public Transform respawnPoint;
+    public GameObject PlayerCar;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void CheckPointUpdate(Transform point)
+    {
+        respawnPoint = point;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Instantiate(PlayerCar, respawnPoint);
+        }
+    }
+}
