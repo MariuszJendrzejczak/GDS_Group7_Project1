a94a84b4ae45c192ae111532a27bfedaeaaa15ea
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-16 16:37:39 +0100

    Trzy prędkości poruszania się świata

--- Assets/Scripts/EnviromentMovement.cs
+++ Assets/Scripts/EnviromentMovement.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class EnviromentMovement : MonoBehaviour
 {
-    [SerializeField][Tooltip("Szybkość poruszania się tego objektu (Default: Podłoga 5, Budynki 3, Góry 1")] private float _speed = 5f;
+    [SerializeField] [Tooltip("Szybkość poruszania się tego objektu (Default: Podłoga 0,98, Budynki 0,6, Góry 0,3")]
+    private float _minSpeed, _speed = 0.98f, _maxSpeed;
+    [SerializeField] [Tooltip("Wartość procentowa zapisana w ułamku dziesiętnym. Skrypt automatycznie przypiszę wartości min i max zmodyfikowane przez tę wartość")]
+    private float _adjustmentValue;
   
     void Start()
     {
-        
+        _minSpeed = _speed * (1f - _adjustmentValue);
+        _maxSpeed = _speed * (1f + _adjustmentValue);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * _speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.left * _maxSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * _minSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * _speed * Time.deltaTime);
+        }
+        
     }
 }
