0d1711a53a726697bb38e5c05f52602f8867f2e5
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-16 17:59:27 +0100

    Dodana funkcja Pauzy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,26 +14,51 @@ public class GameManager : MonoBehaviour
 
     public Transform respawnPoint;
     public GameObject PlayerCar;
+    [SerializeField]
+    private GameObject _pausePanel;
+    private bool _paused = false;
 
     private void Awake()
     {
         _instance = this;
     }
 
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        Pause();
+    }
+
     public void CheckPointUpdate(Transform point)
     {
         respawnPoint = point;
     }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Instantiate(PlayerCar, respawnPoint);
-        }
-    }
+  
+
+    
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.P)&& _paused == false)
+        {
+            _pausePanel.SetActive(true);
+            Time.timeScale = 0;
+            _paused = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.P) && _paused == true)
+        {
+            _pausePanel.SetActive(false);
+            Time.timeScale = 1;
+            _paused = false;
+        }
+
     }
 }
