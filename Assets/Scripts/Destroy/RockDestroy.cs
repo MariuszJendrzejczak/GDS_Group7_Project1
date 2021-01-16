35b2a248c6d32b6cb457fa0088f12009ccf3533a
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-16 16:53:33 +0100

    Dodane ograniczniku ruchu samochodu

--- Assets/Scripts/Destroy/RockDestroy.cs
+++ Assets/Scripts/Destroy/RockDestroy.cs
@@ -7,12 +7,17 @@ public class RockDestroy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Lasser" || collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             PlayerDestroy.Instance.DestroyPlayer();
             Destroy(gameObject);
         }
-            
+        if (collision.tag == "Lasser")
+        {
+            Destroy(gameObject);
+        }
+
+
     }
 
 }
