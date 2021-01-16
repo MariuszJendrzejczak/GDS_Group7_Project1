e7152eb2e70c9895207cd495c2dc3c3529719113
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-16 21:52:01 +0100

    Działające CheckPointy

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -6,6 +6,21 @@ public class CheckPoint : MonoBehaviour
 {
     [SerializeField]
     private bool _isFinal = false;
+
+    private Transform _groundLayer, _paralax1Layer, _paralax2Layer;
+    private float _ground, _paralax1, _paralax2;
+    [SerializeField]
+    private Transform _groundObj, _paralax1Obj, _paralax2Obj;
+
+    private void Start()
+    {
+        _groundObj = GameObject.Find("GroundLayer").GetComponent<Transform>();
+        _paralax1Obj = GameObject.Find("ParalaxLayer1").GetComponent<Transform>();
+        _paralax2Obj = GameObject.Find("ParalaxLayer2").GetComponent<Transform>();
+    }
+
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
@@ -17,7 +32,10 @@ public class CheckPoint : MonoBehaviour
             
         if (collision.tag == "Player")
         {
-            GameManager.Instance.CheckPointUpdate(transform);
+            _ground = _groundObj.transform.position.x;
+            _paralax1 = _paralax1Obj.transform.position.x;
+            _paralax2 = _paralax2Obj.transform.position.x;
+            GameManager.Instance.CheckPointUpdate(_ground, _paralax1, _paralax2);
         }
         
     }
