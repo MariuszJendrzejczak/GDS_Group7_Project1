25258761cd1bf871daaaa2226fdff626c934853c
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-16 13:17:23 +0100

    Ufoki strzelają co 4-10 sekund.

---
+++ Assets/Scripts/Enemy/EnemyLasser.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLasser : MonoBehaviour
+{
+    public enum LasserType { first, second }
+    [SerializeField]
+    private LasserType _type;
+    private float _destroyTime;
+    [SerializeField]
+    [Tooltip("Szybkość poruszania się lasera (Default: 8)")]
+    private float _speed = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _destroyTime = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * _speed * Time.deltaTime);
+    }
+
+    IEnumerator DestroyLasser()
+    {
+
+
+        yield return new WaitForSeconds(_destroyTime);
+        Destroy(gameObject);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerDestroy.Instance.DestroyPlayer();
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Enemy/EnemyShooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Shoot());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 10));
+        Instantiate(_projectile, transform.position, Quaternion.identity);
+        StartCoroutine(Shoot());
+    }
+}
