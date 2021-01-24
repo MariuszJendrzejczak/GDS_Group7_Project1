1e0a6d455ada88d7c3707a5f0f5b9e3e27e67a96
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-24 21:36:59 +0100

    W pełni funkcjonalne Main Manu i Creditsy.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -159,5 +159,7 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    
+
    
 }
---
+++ Assets/Scripts/MainManu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManu : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _creditsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowCredits()
+    {
+        _creditsPanel.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        _creditsPanel.SetActive(false);
+    }
+}
