8a34a4fafbfc9d262bc40a5b57e6b2d849543118
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-27 18:12:26 +0100

    Oskryptowane Kamienie okrągłe

---
+++ Assets/Scripts/Enemy/ActivateRoundRock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateRoundRock : MonoBehaviour
+{
+    [SerializeField]
+    private RoundRock _rock;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            _rock.active = true;
+        }
+    }
+
+
+}
