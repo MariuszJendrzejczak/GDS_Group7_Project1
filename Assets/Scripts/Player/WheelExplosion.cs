a46b8e6fcb7339a5114e88b2e166f19d397d6e7f
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-28 00:44:37 +0100

    są znów wybuchy i respawn automatyczny, ale jeszcze nie do końca to działa.

--- Assets/Scripts/Player/WheelExplosion.cs
+++ Assets/Scripts/Player/WheelExplosion.cs
@@ -15,29 +15,44 @@ public class WheelExplosion : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
 
 
+
+        
+    }
+
+    public void ThrowWheels()
+    {
         switch (_wheel)
         {
             case WitchWheel.back:
                 _rigidbody.AddForce(new Vector2(_backX, _backY), ForceMode2D.Impulse);
                 break;
             case WitchWheel.middle:
-                _rigidbody.AddForce(new Vector2(_middleX,_middleY), ForceMode2D.Impulse);
+                _rigidbody.AddForce(new Vector2(_middleX, _middleY), ForceMode2D.Impulse);
                 break;
             case WitchWheel.front:
                 _rigidbody.AddForce(new Vector2(_frontX, _frontY), ForceMode2D.Impulse);
                 break;
         }
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        switch (_wheel)
+        {
+            case WitchWheel.back:
+                transform.position.Set(-0.0662f, -0.205f,0);
+                break;
+            case WitchWheel.middle:
+                transform.position.Set(-0.0499f, -0.232f, 0);
+                break;
+            case WitchWheel.front:
+                transform.position.Set(-0.1046f, -0.233f, 0);
+                break;
+        }
     }
 }
