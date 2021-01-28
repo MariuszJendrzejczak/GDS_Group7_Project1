106e918e090da7e65c0490086723999557651e08
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-28 18:28:46 +0100

    poprawione kamikadze

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -9,8 +9,8 @@ public class Shooting : MonoBehaviour
     [SerializeField][Tooltip("Ustawienia offsetu dla lasera. Czyli modyfikacja punktu względem pojazdu w którym lasery będą sie pojawiać")]
     private float _posXHorizontalOffset, _posYHorizontalOffset, _posXVerticalOffset, _posYVerticalOffset;
     [SerializeField][Tooltip("Czas przeładowania. Czas między wystrzałami, w którym nie da rady strzelać")]
-    private float _cooldownTime = 1f;
-    private bool _cooldown = false;
+    private float _cooldownTimeHorizontal, _cooldownTimeVertical;
+    private bool _cooldownHorizontal = false, _cooldownVertical = false;
 
     private void Start()
     {
@@ -19,30 +19,48 @@ public class Shooting : MonoBehaviour
 
     private void OnEnable()
     {
-        _cooldown = false;
+        _cooldownHorizontal = false;
+        _cooldownVertical = false;
     }
     // Update is called once per frame
     void Update()
     {
         _OffsetHorizontal = new Vector2(this.transform.position.x + _posXHorizontalOffset, this.transform.position.y + _posYHorizontalOffset);
         _OffsetVertical = new Vector2(this.transform.position.x + _posXVerticalOffset, transform.position.y + _posYVerticalOffset);
-        if (_cooldown == false)
-        {
-            if (Input.GetKeyDown(KeyCode.L))
+       
+        
+       if (Input.GetKeyDown(KeyCode.L))
+       {
+            if (_cooldownHorizontal == false)
             {
                 Instantiate(_lasserHorizontal, _OffsetHorizontal, Quaternion.identity);
-                Instantiate(_lasserVertical, _OffsetVertical, Quaternion.identity);
-                _cooldown = true;
+                _cooldownHorizontal = true;
                 AudioManager.Instanse.AudioCarShoot();
-                StartCoroutine(CoolDown());
+                StartCoroutine(CoolDownHorizontal());
             }
-        }
+            if (_cooldownVertical == false)
+            {
+                Instantiate(_lasserVertical, _OffsetVertical, Quaternion.identity);
+                _cooldownVertical = true;
+                AudioManager.Instanse.AudioCarShoot();
+                StartCoroutine(CoolDownVertical());
+            }
+
+
+
+       }
+     
        
     }
 
-    IEnumerator CoolDown()
+    IEnumerator CoolDownHorizontal()
     {
-        yield return new WaitForSeconds(_cooldownTime);
-        _cooldown = false;
+        yield return new WaitForSeconds(_cooldownTimeHorizontal);
+        _cooldownHorizontal = false;
+    }
+    IEnumerator CoolDownVertical()
+    {
+        yield return new WaitForSeconds(_cooldownTimeVertical);
+        _cooldownVertical = false;
     }
 }
