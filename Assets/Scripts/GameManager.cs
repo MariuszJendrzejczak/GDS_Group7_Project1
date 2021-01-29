6ee97e81a255dda199377ddaa89dfbfbcbe6dfe3
Mariusz Jendrzejczak <jendrzejczak.mariusz@gmail.com>
2021-01-29 17:39:06 +0100

    poprawki paralaxy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         UpdateEnviroment();
-        Pause();
+       // Pause();
         GameTimer();
 
         _sceneIndex = SceneManager.GetActiveScene().buildIndex;
